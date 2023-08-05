eb489b8f252debf9e393487b8bcd300214dd8d92
Phahladira-Moja <phahladira@patala.co.za>
2023-08-05 13:21:39 +0200

    Added the Faq section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import heroimg03 from "../assets/images/hero-img03.png";
 import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
+import faqImg from "../assets/images/faq-img.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
@@ -13,6 +14,7 @@ import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Service/ServiceList";
 import DoctorsList from "../components/Doctors/DoctorsList";
+import FaqList from "../components/Faq/FaqList";
 
 const Home = () => {
   return (
@@ -289,6 +291,26 @@ const Home = () => {
         </div>
       </section>
       {/* ============= our great doctors ================= */}
+
+      {/* ============= faq section ================= */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="faq-image" />
+            </div>
+
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ============= faq section end ================= */}
     </>
   );
 };
