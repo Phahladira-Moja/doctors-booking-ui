f7c675d34b282d4fbbdda4670ae0953019cefea9
Phahladira-Moja <phahladira@patala.co.za>
2023-08-05 13:53:38 +0200

    removed footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
 const Footer = () => {
   return <div>Footer</div>;
 };
